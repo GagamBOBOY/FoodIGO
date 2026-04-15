e611dd817d9c27a3d4d0e5c0e9b08d3660361ac9
GagamBOBOY <salesezekiel057@gmail.com>
2026-04-15 17:03:35 +0800

    Implement header color change and scroll up button
    Added header background color change on scroll and scroll up button functionality.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 // Swiper js
-var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      // grabCursor: true}
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
 
 // Nav open close
 
 
 // Change header bg color
-
+window.addEventListener("scroll", () => {
+ const scrollY = window.pageYOffset;
+ 
+ if(scrollY > 5){
+    document.querySelector("header").classList.add("header-active");
+ }else{
+ document.querySelector("header").classList.remove("header-active");
+ }
+ console.log
+})
 
 // Scroll up button
-  
+  const scrollUpBtn = document.querySelector('.scrollUp-btn');
+
+  if(scrollY > 250){
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  }else{
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
   
   
 // Nav link indicator
